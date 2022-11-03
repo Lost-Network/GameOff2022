f3a5d2ea33dec24cfa1e44940e400d7bf9b7a99e
Chryen <squirtlexomg@yahoo.com>
2022-11-03 02:52:40 -0500

    (refactor) remove useless code and spacing

--- Assets/Scripts/UI/SoundboardBGMs.cs
+++ Assets/Scripts/UI/SoundboardBGMs.cs
@@ -20,36 +20,36 @@ public class SoundboardBGMs : MonoBehaviour
 
     void Awake()
     {
-        BGM = GameObject.Find("Soundboard/BGM");
-        StatsManager = GameObject.Find("StatsManager");
-        Theme = GameObject.Find("Theme");
+      BGM = GameObject.Find("Soundboard/BGM");
+      StatsManager = GameObject.Find("StatsManager");
+      Theme = GameObject.Find("Theme");
 
-        // DontDestroyOnLoad(transform.gameObject);
+      // DontDestroyOnLoad(transform.gameObject);
 
-        // if (playerInstance == null)
-        // {
-        //     playerInstance = this;
-        // }
-        // else
-        // {
-        //     DestroyObject(gameObject);
-        // }
+      // if (playerInstance == null)
+      // {
+      //     playerInstance = this;
+      // }
+      // else
+      // {
+      //     DestroyObject(gameObject);
+      // }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < BGM.transform.childCount; i++)
-        {
-            Sounds[i] = BGM.transform.GetChild(i).gameObject;
-        }
+      for (int i = 0; i < BGM.transform.childCount; i++)
+      {
+        Sounds[i] = BGM.transform.GetChild(i).gameObject;
+      }
 
-        slider = GetComponent<Slider>();
-        BGMSliderValue = StatsManager.GetComponent<StatsManager>().BGMSliderValue;
-        if( slider != null && Theme != null )
-            slider.onValueChanged.AddListener( value => Theme.GetComponent<VolumeValueChange>().SetVolume(BGMSliderValue)) ;
-        else
-            Debug.LogError("Either the slider is not attached to the GO or no OptionsSwitch in the scene" ) ;
+      slider = GetComponent<Slider>();
+      BGMSliderValue = StatsManager.GetComponent<StatsManager>().BGMSliderValue;
+      if( slider != null && Theme != null )
+        slider.onValueChanged.AddListener( value => Theme.GetComponent<VolumeValueChange>().SetVolume(BGMSliderValue)) ;
+      else
+        Debug.LogError("Either the slider is not attached to the GO or no OptionsSwitch in the scene" ) ;
     }
 
     // Update is called once per frame
--- Assets/Scripts/UI/StatsManager.cs
+++ Assets/Scripts/UI/StatsManager.cs
@@ -18,16 +18,16 @@ public class StatsManager : MonoBehaviour
 
     void Awake()
     {
-        DontDestroyOnLoad(transform.gameObject);
+      DontDestroyOnLoad(transform.gameObject);
 
-        if (playerInstance == null)
-        {
-            playerInstance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+      if (playerInstance == null)
+      {
+        playerInstance = this;
+      }
+      else
+      {
+        Destroy(gameObject);
+      }
     }
     // Start is called before the first frame update
     void Start()
@@ -38,24 +38,23 @@ public class StatsManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (GameObject.Find("SoundBoard") != null)
-        {
-            SoundBoard = GameObject.Find("SoundBoard");
+      if (GameObject.Find("SoundBoard") != null)
+      {
+        SoundBoard = GameObject.Find("SoundBoard");
+      }
+      // Mainly keeps track of slider values
+      if (GameObject.Find("BGMSlider") != null)
+      {
+        BGMSlider = GameObject.Find("BGMSlider");
+        if (!BGMSliderBool) {
+          BGMSliderInitialValue = BGMSlider.GetComponent<Slider>().value;
+          BGMSliderBool = true;
         }
-
-        if (GameObject.Find("BGMSlider") != null)
-        {
-            BGMSlider = GameObject.Find("BGMSlider");
-            if (!BGMSliderBool) {
-              BGMSliderInitialValue = BGMSlider.GetComponent<Slider>().value;
-              BGMSliderBool = true;
-            }
-            if (BGMSlider.GetComponent<Slider>().value == 0.5f && BGMSliderValue != 0.5f) {
-              BGMSlider.GetComponent<Slider>().value = BGMSliderValue;
-            }
-            BGMSliderValue = BGMSlider.GetComponent<Slider>().value;
+        if (BGMSlider.GetComponent<Slider>().value == 0.5f && BGMSliderValue != 0.5f) {
+          BGMSlider.GetComponent<Slider>().value = BGMSliderValue;
         }
+        BGMSliderValue = BGMSlider.GetComponent<Slider>().value;
+      }
 
     }
 }
