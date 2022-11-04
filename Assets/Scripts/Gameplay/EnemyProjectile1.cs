208724ab95b716a974f8b54e83f2c013a4a0afa8
BerzerkHere <44713289+BerzerkHere@users.noreply.github.com>
2022-11-04 01:46:00 -0500

    fixed

--- Assets/Scripts/Gameplay/EnemyProjectile1.cs
+++ Assets/Scripts/Gameplay/EnemyProjectile1.cs
@@ -19,20 +19,20 @@ public class EnemyProjectile1 : MonoBehaviourPunCallbacks, IPunObservable
 
     private void OnCollisionEnter2D(Collision2D coll)
     {
-        if (coll.gameObject.tag != "Player")
-        {
-            if(GetComponent<PhotonView>().AmOwner)
-            {
-                PhotonNetwork.Destroy(gameObject);
-                //photonView.RPC("DestroyMe", RpcTarget.AllBuffered);
-            }
-            return;
-        }
-        else if (coll.gameObject.GetComponent<PhotonView>().AmOwner)
+        if (coll.gameObject.tag == "Player" && coll.gameObject.GetComponent<PhotonView>().AmOwner)
         {
             coll.gameObject.GetComponent<PlayerStats>().DecreaseHealth(damage);
             photonView.RPC("DestroyMe", RpcTarget.MasterClient);
         }
+        //else
+        //{
+        //    if (GetComponent<PhotonView>().AmOwner)
+        //    {
+        //        PhotonNetwork.Destroy(gameObject);
+        //        //photonView.RPC("DestroyMe", RpcTarget.AllBuffered);
+        //    }
+        //    return;
+        //}
     }
     [PunRPC]
     void DestroyMe()
