716d5a98ebc9ea106628f92754b8fbaea810ac7f
BerzerkHere <44713289+BerzerkHere@users.noreply.github.com>
2022-11-04 16:08:58 -0500

    added cliches

---
+++ Assets/Scripts/ScriptableObjects/ScriptableCliche.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Cliche", menuName = "Cliche")]
+public class ScriptableCliche : ScriptableObject
+{
+    public string Name;
+    [TextArea(1, 3)]
+    public string Description;
+    public int id;
+    //public Sprite cardSprite;
+    //public Sprite Jewel;
+
+    //public CardColor cardColor;
+    //public int cardMana;
+    //public int cardCooldown;
+    //public int cardAttack;
+    //public int cardHealth;
+    //public int cardCost;
+    ////public int color;
+    ////public GameObject skill;
+    //public int cardID;
+    //public bool needsTarget;
+    //public bool combo;
+    //public bool cast;
+}
