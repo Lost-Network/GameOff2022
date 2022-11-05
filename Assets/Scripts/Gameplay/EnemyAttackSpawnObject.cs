e8b7d607b9ca6e1ddddd09ae0825bbe2d0fef69d
Dylan <dylanfshannon@gmail.com>
2022-11-04 20:44:03 -0700

    I changed when the projectile script spawns something

--- Assets/Scripts/Gameplay/EnemyAttackSpawnObject.cs
+++ Assets/Scripts/Gameplay/EnemyAttackSpawnObject.cs
@@ -24,7 +24,7 @@ public class EnemyAttackSpawnObject : MonoBehaviour
         }
         else
         {
-            if (objectSpawnTimer >= objectSpawnTimerCap && GetComponent<MovetowardsNearestPlayer>().combatState != 1)
+            if (objectSpawnTimer >= objectSpawnTimerCap && GetComponent<MovetowardsNearestPlayer>().combatState == 2)
             {
                 objectSpawnTimer = 0f;
                 GameObject spawnedObject = PhotonNetwork.Instantiate(objectToSpawn, this.transform.position, Quaternion.identity);
