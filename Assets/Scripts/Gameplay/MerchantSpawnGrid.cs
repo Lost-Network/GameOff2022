409593fafdd71134140c90134ff94e9af300beae
Chryen <squirtlexomg@yahoo.com>
2022-11-05 07:10:18 -0500

    (feature) add grid and damsel

--- Assets/Scripts/Gameplay/MerchantSpawnGrid.cs
+++ Assets/Scripts/Gameplay/MerchantSpawnGrid.cs
@@ -1,42 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class MerchantSpawnGrid : MonoBehaviour
 {
+    public Transform[] spawnPoints;
 
-    public Transform[] spawnPoints; 
-    public GameObject[] merchPrefabs; 
+    public GameObject[] merchPrefabs;
 
     public GameObject MerchantDialogueBox;
+
+    public string objectToSpawn;
 
     public bool textBox = false;
 
     void Awake()
     {
-      // MerchantDialogueBox = GameObject.Find("MerchantDialogueBox");
+        // MerchantDialogueBox = GameObject.Find("MerchantDialogueBox");
     }
 
-    void Start() 
+    void Start()
     {
-      // MerchantDialogueBox.SetActive(textBox);
+        // MerchantDialogueBox.SetActive(textBox);
     }
 
-    void Update() 
+    void Update()
     {
-      if (Input.GetKeyDown("m")){
-        int merch = Random.Range(0, merchPrefabs.Length); 
-        int randSpawnPoint = Random.Range(0, spawnPoints.Length); 
-        Instantiate(merchPrefabs[merch], spawnPoints[randSpawnPoint].position, transform.rotation); 
-      }
-      if (Input.GetKeyDown("f")){
-        textBox = !textBox;
-        MerchantDialogueBox.SetActive(textBox);
-      }
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            return;
+        }
+        if (Input.GetKeyDown("m"))
+        {
+            int merch = Random.Range(0, merchPrefabs.Length);
+            int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+            GameObject go =
+                PhotonNetwork
+                    .Instantiate(objectToSpawn,
+                    spawnPoints[randSpawnPoint].position,
+                    transform.rotation);
+        }
+        if (Input.GetKeyDown("f"))
+        {
+            textBox = !textBox;
+            MerchantDialogueBox.SetActive (textBox);
+        }
     }
 
-    void SpawnMerchant() 
+    void SpawnMerchant()
     {
-    
     }
 }
