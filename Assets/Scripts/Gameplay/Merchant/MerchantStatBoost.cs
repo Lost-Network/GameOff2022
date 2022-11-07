59ab483b8119fd07cdd94214dd94a12928488ca3
Chryen <squirtlexomg@yahoo.com>
2022-11-07 09:34:52 -0600

    (update) add clickable buttons and change debug

--- Assets/Scripts/Gameplay/Merchant/MerchantStatBoost.cs
+++ Assets/Scripts/Gameplay/Merchant/MerchantStatBoost.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class MerchantStatBoost : MonoBehaviour
 {
+
+    public GameObject MerchantShop;
+
+    public int attackBoost = 1;
+    public int healthBoost = 1;
+    public int speedBoost = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +21,21 @@ public class MerchantStatBoost : MonoBehaviour
     {
         
     }
+
+    public void AddAttackBoost(){
+      Debug.Log("Added +1 attack!");
+      MerchantShop.SetActive(false);
+    }
+
+    public void AddHealthBoost(){
+      Debug.Log("Added +1 health!");
+      MerchantShop.SetActive(false);
+    }
+
+    public void AddSpeedBoost(){
+      Debug.Log("Added +1 speed!");
+      MerchantShop.SetActive(false);
+    }
+
+
 }
