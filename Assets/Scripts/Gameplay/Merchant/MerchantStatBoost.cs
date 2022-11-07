273ef8327f960e0182c886de2811c7fdb0d22237
Chryen <squirtlexomg@yahoo.com>
2022-11-07 09:48:31 -0600

    (feat) add button sound to merch menu

--- Assets/Scripts/Gameplay/Merchant/MerchantStatBoost.cs
+++ Assets/Scripts/Gameplay/Merchant/MerchantStatBoost.cs
@@ -7,9 +7,16 @@ public class MerchantStatBoost : MonoBehaviour
 
     public GameObject MerchantShop;
 
+    public Transform Soundboard;
+
     public int attackBoost = 1;
     public int healthBoost = 1;
     public int speedBoost = 1;
+
+    void Awake()
+    {
+        Soundboard = GameObject.Find("Soundboard/SFX").transform;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -24,16 +31,19 @@ public class MerchantStatBoost : MonoBehaviour
 
     public void AddAttackBoost(){
       Debug.Log("Added +1 attack!");
+      Soundboard.GetChild(0).GetComponent<AudioSource>().Play();
       MerchantShop.SetActive(false);
     }
 
     public void AddHealthBoost(){
       Debug.Log("Added +1 health!");
+      Soundboard.GetChild(0).GetComponent<AudioSource>().Play();
       MerchantShop.SetActive(false);
     }
 
     public void AddSpeedBoost(){
       Debug.Log("Added +1 speed!");
+      Soundboard.GetChild(0).GetComponent<AudioSource>().Play();
       MerchantShop.SetActive(false);
     }
 
