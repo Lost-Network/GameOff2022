f7884755e63c94e63860089d56edf78234dc1b80
Chryen <squirtlexomg@yahoo.com>
2022-11-09 10:08:07 -0600

    (feat) add the Big Cheese Boss and cheese puddles

---
+++ Assets/Scripts/Gameplay/Enemy/EnemySlowPuddle.cs
@@ -0,0 +1,57 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySlowPuddle : MonoBehaviourPunCallbacks, IPunObservable
+{
+    [SerializeField]
+    private float timer = 10f;
+
+    PhotonView view;
+
+    private void Awake()
+    {
+        view = GetComponent<PhotonView>();
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (0 > timer)
+        {
+            PhotonNetwork.Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag != "Player")
+        {
+            return;
+        }
+
+        if (coll.gameObject.GetComponent<PhotonView>().AmOwner)
+        {
+            coll.gameObject.GetComponent<Movement>().speed = 0.5f;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag != "Player")
+        {
+            return;
+        }
+
+        if (coll.gameObject.GetComponent<PhotonView>().AmOwner)
+        {
+            coll.gameObject.GetComponent<Movement>().speed = 2;
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+
+    }
+}
