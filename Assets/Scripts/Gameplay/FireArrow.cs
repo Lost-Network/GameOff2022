f02b048675d13e40fafc41f4c8d6b1d74a11f18e
Chryen <squirtlexomg@yahoo.com>
2022-11-30 18:05:04 -0600

    (feat) make fire arrow pretty

--- Assets/Scripts/Gameplay/FireArrow.cs
+++ Assets/Scripts/Gameplay/FireArrow.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class FireArrow : MonoBehaviour
 {
@@ -9,11 +10,35 @@ public class FireArrow : MonoBehaviour
     public float time = 0f;
     private bool killMe = false;
 
+    public GameObject[] shotDirections;
+    private int bulletsShot;
+    private bool bigBulletActive;
+    private float bigBulletTimer;
+
     // Start is called before the first frame update
     void Start()
     {
         //Attack = this.GetComponentInParent<PlayerStats>().Attack + baseDamage;
         //Debug.Log(Attack);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        object[] instanceData = new object[1];
+        instanceData[0] = 3;
+        for (int i = 0; i < shotDirections.Length ; i++)
+        {
+            GameObject go = PhotonNetwork.Instantiate("FireProjectile", shotDirections[i].transform.position, shotDirections[i].transform.rotation, 0, instanceData);
+            go.GetComponent<Rigidbody2D>().AddForce(go.transform.up * 10f);
+            bulletsShot++;
+        }
+        if (bulletsShot >= shotDirections.Length - 1)
+        {
+            timer = 2;
+            bulletsShot = 0;
+        }
     }
 
     // Update is called once per frame
